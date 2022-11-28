a1113773a4fd9095641c3d9d2bae9142f4995496
Juha Halmu <jhalmu@users.noreply.github.com>
2022-11-28 17:07:32 +0200

    Adding recharts back to repo if want to use

--- src/recharts/Pies/BasePieChart.jsx
+++ src/recharts/Pies/BasePieChart.jsx
@@ -1,7 +1,7 @@
 import "../styles/styles.css";
 //
 /// DATA
-import data from '../../_data/BaseData.json';
+import data from '../../components/_data/BaseData.json';
 //import React, { useCallback, useState } from "react";
 import { Cell, Pie, PieChart, ResponsiveContainer, Tooltip } from "recharts";
 
